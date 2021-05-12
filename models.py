c18a139bacb70bbb01d85fd08aa907da38f47510
zurgeg <zurgeg@gmail.com>
2021-05-12 08:28:47 -0400

    Update models and bot

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Table, Column, Integer, MetaData
+from sqlalchemy import Table, Column, Integer
 from sqlalchemy.orm import sessionmaker
-meta = MetaData()
 
-users = Table(
-    'users', meta,
-    Column('id', Integer, primary_key = True),
-    Column('strikes', Integer),
-    Column('points', Integer)
-)
-
-def init_db(engine):
-    meta.create_all(engine)
 from bot import engine
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    strikes = Column(Integer, primary_key=True)
+    points = Column(Integer, primary_key=True)
+
+
 Session = sessionmaker(bind=engine)
 session = Session()
