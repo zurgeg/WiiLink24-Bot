4455861fe6bd3fc97cbfbe7ed59b6af789de13ae
zurgeg <zurgeg@gmail.com>
2021-05-12 10:50:09 -0400

    Use rules from config

--- config.example.py
+++ config.example.py
@@ -1,4 +1,4 @@
 token = 'token moment'
 plugins = []
 db_url = 'sqlite:///amongus.db'
-
+rules = {1: 'no'}
--- plugins/moderation.py
+++ plugins/moderation.py
@@ -3,30 +3,12 @@ from discord_slash import cog_ext, SlashContext
 from discord import User
 import time
 import discord
+import config
 from models import Users, session
 class Moderation(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.rules = {
-            1:"Use common sense! If you think it is wrong than don't post it!.",
-            2:'No NSFW Content! No channels are marked as NSFW and there are people under 18 here.',
-            3:'Keep advertising inside <#773219940647698462>',
-            4:'Keep the chat in English.',
-            5:'No drama! If you want to have drama, do it in DMs.',
-            6:'Keep bot commands and other memes and other spammy stuff to <#750623633151492177>.',
-            7:'No user bots. These are in some cases against the Discord ToS and if you need a bot for something use one of the bots already in the server.',
-            8:"Don't send copyrighted content here, you can still talk about it though.",
-            9:'MarioCube is the exception to above as the databases wad files are the only thing left of a lot of those apps and without them the apps would be lost to time.',
-            10:'Don\'t attack anyone unless you have a valid reason to',
-            11:'Only post junk in <#750623633151492177>',
-            12:'People are people, no discrimination of any sort',
-            13:'Do not ping staff unless needed',
-            14:'Try to limit swearing',
-            15:'Do not try to exploit the server though use of oversights in bots, leaked accounts etc.',
-            24:'WiiConnect',
-            418:'The requsted entity is short and stout.',
-            9999:'You found a secret!'
-        }
+        self.rules = config.rules
         self.afk_users = {}
         self.rule_limited_users = {}
     @commands.has_permissions(ban_members=True)
