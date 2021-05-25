710d676e748cb4484b10f9ef5ffc0a6aa9721a26
zurgeg <zurgeg@gmail.com>
2021-05-24 22:36:52 -0400

    Whoops

--- migrations/versions/001_Add_User_Table.py
+++ migrations/versions/001_Add_User_Table.py
@@ -11,9 +11,10 @@ users = Table('users', meta,
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    meta.
+    meta.bind = migrate_engine
+    users.create()
 
 
 def downgrade(migrate_engine):
-    # Operations to reverse the above upgrade go here.
-    pass
+    meta.bind = migrate_engine
+    users.drop()
